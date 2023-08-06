f772045711a706dc1152cbac06464c3bf2c3d4d1
Ernesto Canquiz <fdsoil123@gmail.com>
2023-08-06 17:41:44 -0400

    feat: Add generate-pwa-assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vue-todo-pwa/',
   plugins: [
     vue(),
+    VitePWA({      
+      manifest: { 
+        icons: [{
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        }, {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        }, {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'  
+        }, {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }]
+      }
+    })
   ],
   resolve: {
     alias: {
